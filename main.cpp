655eec7d605a168d5f45a587914b782f4a4a07c5
Antonio Almeida <alfecjo@hotmail.com>
2023-01-16 19:33:49 -0300

    validacoes iniciais do ambiente em Keil, primeira compilacao Ok.

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include "arm_book_lib.h"
+#include "mbed.h"
+
+int main() {
+
+  DigitalIn enterButton(BUTTON1);
+  DigitalIn gasDetector(D2);
+  DigitalIn overTempDetector(D3);
+  DigitalIn aButton(D4);
+  DigitalIn bButton(D5);
+  DigitalIn cButton(D6);
+  DigitalIn dButton(D7);
+
+  DigitalOut alarmLed(LED1);
+  DigitalOut systemBlockedLed(LED2);
+  DigitalOut incorrectCodeLed(LED3);
+
+  gasDetector.mode(PullDown);
+  overTempDetector.mode(PullDown);
+  aButton.mode(PullDown);
+  bButton.mode(PullDown);
+  cButton.mode(PullDown);
+  dButton.mode(PullDown);
+
+  alarmLed = OFF;
+  systemBlockedLed = OFF;
+  incorrectCodeLed = OFF;
+
+  bool alarmState = OFF;
+  int numberOfIncorrectCodes = 0;
+
+  while (true) {
+    if (gasDetector || overTempDetector) {
+      alarmState = ON;
+    }
+    alarmLed = alarmState;
+    if (numberOfIncorrectCodes < 3) {
+      if (aButton && bButton && cButton && dButton && !enterButton) {
+        incorrectCodeLed = OFF;
+      }
+      if (enterButton && !incorrectCodeLed && alarmState) {
+        if (aButton && bButton && !cButton && !dButton) {
+          numberOfIncorrectCodes = 0;
+          alarmState = OFF;
+        } else {
+          incorrectCodeLed = ON;
+          numberOfIncorrectCodes = numberOfIncorrectCodes + 1;
+        }
+      }
+    } else {
+      systemBlockedLed = ON;
+    }
+  }
+}
